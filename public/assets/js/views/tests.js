673c3e746b4c2ded4855c40e5ddb1fac5c2bfe7e
ThiagoJVLF <thiago_f@aeiou.pt>
2015-05-12 02:08:11 +0100

    (continuação) de construção das janelas dos testes, seguindo um pouco a versão da app móvel..

--- public/assets/js/views/tests.js
+++ public/assets/js/views/tests.js
@@ -1,9 +1,44 @@
 window.TestsView = Backbone.View.extend({
-  events: {},
+  events: {
+    "click #btnTestNew":"newTest",
+
+  },
+
+  newTest:function(){
+    console.log("Queres novo teste?? ");
+
+    //deverá aparecer um Modal para escolher o tipo de teste a criar.
+    //Teste:
+
+
+  },
+
   initialize: function() {},
 
   render: function() {
     $(this.el).html(this.template());
+
+
+    modem('GET', 'tests', function(data) {
+      console.log("nº de Testes: ", data.length);
+      $('#testBadge').text(data.length);
+      var s='';
+      for(i=0;i<data.length;i++){
+        s+= '<button id="'
+          + data[i].doc._id
+          + '"  name="' + data[i].doc._id
+          +'"  type="button" style="height:50px; background-color: #53BDDC; color: #ffffff;"'
+          +' class="btn btn-lg btn-block" >'
+          +' <img src="../img/testLista.png"  style="height:25px;" > '
+          + data[i].doc.titulo + ' - DD.MM.AAAA </button>';
+      }
+      $('#testsContent').html(s);
+
+      //$('#testsContent', self.el).html(JSON.stringify(data));
+    }, function(error) {
+      console.log('Error getting tests list!');
+    });
+
     return this;
   }
 });
