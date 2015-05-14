6a17cb24a1e37de0ec88d20ca7ceea86c5310e54
ArturGomesIPT <artur.s.gomes@gmail.com>
2015-05-14 13:30:05 +0100

    Escolas
    nesta altura temos as bases das inserções preparadas

--- public/assets/js/views/schools.js
+++ public/assets/js/views/schools.js
@@ -1,21 +1,63 @@
 window.SchoolsView = Backbone.View.extend({
-  events: {},
+  events: {
+     "click #btnSchoolNew":"newEscola",
+   },
+    
+newEscola: function(e) {
+    e.preventDefault();
+    app.navigate('/schools/new', {
+    trigger: true
+    }); 
+  },    
+    
   initialize: function() {},
 
-  render: function() {
-    var self = this;
-      
+   render: function() {
     $(this.el).html(this.template());
-      
-     modem('GET', 'schools', function(data) {
-      $('#subcontent', self.el).html(JSON.stringify(data));
-    }, function(error) {
-      console.log('Error getting school list!');
-    });  
-      
-      
-    return this;
-  }
-});
 
+        
+    modem('GET', 'schools', function(data) {
+      $('#schoolsBadge').text(data.length);
+      var s='';
+      for(i=0;i<data.length;i++){
+        s+= '<button id="'
+          + data[i].doc._id
+          + '"  name="' + data[i].doc._id
+          +'"  type="button" style="height:50px; background-color: #53BDDC; color: #ffffff;"'
+          +' class="btn btn-lg btn-block" >'
+          +' <img src="http://localhost:5984/dev_escolas/'+data[i].doc._id+'/escola.png"  style="height:25px;" > '
+          + data[i].doc.nome + '</button>';
+      }
+
+      $('#schoolsContent').html(s);
+
+      var url='';
+
+        console.log(data[0].doc.nome);
+        console.log(data[0].doc._attachments);
+        
+
+        var conteudo='';
+            conteudo+= '<img src="http://localhost:5984/dev_alunos/'+data[0].doc._id+'/escola.png"  style="height:250px;">';
+            conteudo+= '<br><label>'+data[0].doc.nome+' </label><br>';
+            conteudo+= '<label>'+data[0].doc.morada+'</label><br>';
+           // conteudo+= '<label>'+data[0].doc.turmas+'</label><br>';
+        
+
+        $('#schoolsPreview').html(conteudo);
+
+
+        
+        
+    }, function(error) {
+      console.log('Error getting schools list!');
+    });
+
+    return this;
+  },
+  
+ 
+    
+    
+});
 
