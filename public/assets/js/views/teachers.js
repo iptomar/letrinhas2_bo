d17abcfb38b55d722b60a6f389ccc101f8eef2a6
ThiagoJVLF <thiago_f@aeiou.pt>
2015-05-14 06:19:43 +0100

    upDate7 - consulta dos professores e respetivas turmas, acesso para os formulários de inserção e alteração. - Fica a faltar a filtragem, para 2ª Fase..

--- public/assets/js/views/teachers.js
+++ public/assets/js/views/teachers.js
@@ -1,7 +1,7 @@
 window.TeachersView = Backbone.View.extend({
   events: {
-     "click #btnTeachersNew": "newTeacher",
-     "click .profSelec":"mudaProf",
+    "click #btnTeachersNew": "newTeacher",
+
   },
 
   initialize: function() {
@@ -49,6 +49,18 @@ window.TeachersView = Backbone.View.extend({
 
       $('#teachersContent').html(s);
 
+      self.getTurmas();
+
+      //Criar Evento
+      var myEl = document.getElementsByClassName('profSelec');
+      for(j=0;j<myEl.length;j++){
+        myEl[j].addEventListener('click', function() {
+                      self.mudaProf(this);
+                    }, false);
+      };
+
+
+
       // Dúvidas:
       // Já tenho o attachements, mas não o consigo usar... como usar e colocar no html??
 
@@ -91,21 +103,109 @@ window.TeachersView = Backbone.View.extend({
     return this;
   },
 
-  mudadProf
+/*
+var myEl = document.getElementByClass('AU');
+myEl.addEventListener('click', function() {
+              self.mudaProf(this);
+            }, false);
+*/
+
+  getTurmas:function(){
+    var self=this;
+    modem('GET','schools', function(json){
+      var s='<div align="left">';
+      var has=false;
+      var contTurmas=0;
+      console.log("n Escolas: "+json.length);
+      for(i=0; i< json.length; i++){
+        var escola="<div><div class='col-md-7'><label>"+json[i].doc.nome+': </label></div>';
+        var turma='';
+        for(j=0; j< json[i].doc.turmas.length; j++){
+
+          for(k=0; k<json[i].doc.turmas[j].professores.length; k++){
+            var g=""+json[i].doc.turmas[j].professores[k].id;
+            var f=""+$("#profEmail").text();
+            if(f == g){
+              has=true;
+            }
+          }
+          if(has){
+            turma+='<span> - '+json[i].doc.turmas[j].nome+'</span><br>';
+            has=false;
+            contTurmas++;
+          }
+        }
+        if(turma.length>1){
+          s+=escola+"<div class='col-md-4'>"+turma+"<br></div></div><br>";
+        }
+
+      }
+
+      if(contTurmas>0){
+        s+="</div>";
+        $("#SchoolTable").html(s);
+        $("#prfSchool").text($("#profNome").text()+", tem "+contTurmas+" turmas associadas.");
+      }
+      console.log($("#profEmail").text());
+      console.log(json.length);
+      //console.log('idProf: '+ json[0].doc.turmas[0].professores[0].id);
+
+
+    },
+    function(error) {
+      console.log('Error getting schools list!');
+    });
+
+
+
+  },
+
+  mudaProf:function(obj){
+    var self=this;
+    //vai buscar os dados do professor:
+    modem('GET','teachers/'+obj.id, function(json){
+      $('#teachersPreview').html(self.enchePreview(json));
+      var myEl = document.getElementById('btnTeachersEdit');
+      myEl.addEventListener('click', function() {
+                    self.editTeacher(this);
+                  }, false);
+
+      self.getTurmas();
+
+    },
+    function(error) {
+      console.log('Error getting teachers list!');
+    });
+
+  },
 
   enchePreview: function(documnt){
     var html='';
 
-    html+= '<img src="http://localhost:5984/dev_professores/'+documnt._id+'/prof.png"  style="height:250px;">';
-    html+= '<br><span>Nome: <label>'+documnt.nome+' </label></span>';
-    html+= '<br><span>E-mail: <label>'+documnt._id+' </label></span>';
+    html+= '<img src="http://localhost:5984/dev_professores/'+documnt._id+'/prof.png"  style="height:220px;">';
+    html+= '<br><div align=left class="col-md-8"><span>Nome: <label id="profNome">'+documnt.nome+' </label></span>';
+    html+= '<br><span>E-mail: <label id="profEmail">'+documnt._id+'</label></span>';
     html+= '<br><span>Telefone: <label>'+documnt.telefone+' </label></span>';
-    html+= '<br><span>Tipo de utilizador: <label>'+documnt.tipo+' </label></span><br>';
+    html+= '<br><span>Tipo de utilizador: <label>'+documnt.tipo+' </label></span></div>';
+    //Botão para Editar
+    html+='<div align=right class="col-md-3"><br><br><br>'
+        +'<button id="btnTeachersEdit" name='+documnt._id+' class="btn btn-warning" style="font-size:10px">'
+        +'<span class="glyphicon glyphicon-pencil" style="color:#ffff00;"></span>'
+        +' Editar dados'
+        +'</button>'
+        +'</div><br>';
 
     // falta ir buscar à BD as escolas onde o professor leciona e turmas
+    html+= '<div id="prfSchool" class="col-md-12" align=left><hr>'+documnt.nome+' não tem turmas associadas.</div><br>';
+    html+= '<div id="SchoolTable" class="col-md-12" align="center" style="max-height:220px; overflow:auto"></div>';
+
+
+
+    //html+= '<hr>';
+
+
 
     return html;
-    //data[i].doc)
   },
 
 
@@ -114,6 +214,14 @@ window.TeachersView = Backbone.View.extend({
     app.navigate('/teachers/new', {
     trigger: true
     });
+  },
+
+  editTeacher: function (obj) {
+    //Variavel a enviar, para depois poder buscar os dados do professor a editar
+    window.localStorage.setItem("ProfEditar", obj.name);
+    app.navigate('/teachers/edit', {
+    trigger: true
+  });
   },
 
 
